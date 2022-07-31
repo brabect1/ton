94f26462d2a0fd1897c849e46295ae83bf9c5b50
Tomas Brabec <xtbr123@gmail.com>
2022-07-31 18:20:39 +0200

    Ported tests from `huddle.test`.
    Added tests for various huddle structures as unit tested in Tcllib's `yaml`
    package.

--- ton2huddle.tcl
+++ ton2huddle.tcl
@@ -15,45 +15,51 @@
 lappend auto_path .
 package require ton;
 
-set json {{"foo":"bar","lst": [1,2,"3"],"obj": {"a":1,"b":2}}};
-
-set ton [ton::json2ton $json];
-puts "$ton";
-
-## puts "[lrange $ton 1 end]";
-## set l [dict get [lrange $ton 1 end] "foo"];
-## puts "$l";
-
-#set d [ton::a2dict $ton];
-#puts "[ton::2list::get $ton "foo" "bar"]";
-
-namespace eval ton::json2dict {
-    set d [eval $ton];
-    puts "$d";
-}
+## set json {{"foo":"bar","lst": [1,2,"3"],"obj": {"a":1,"b":2}}};
+## 
+## set ton [ton::json2ton $json];
+## set ton {o a [s b] c [s d]}
+## puts "$ton";
+## 
+## ## puts "[lrange $ton 1 end]";
+## ## set l [dict get [lrange $ton 1 end] "foo"];
+## ## puts "$l";
+## 
+## #set d [ton::a2dict $ton];
+## #puts "[ton::2list::get $ton "foo" "bar"]";
+## 
+## namespace eval ton::json2dict {
+##     set d [eval $ton];
+##     puts "$d";
+## }
 
 namespace eval ton::2huddle {
     proc s v {return [list s [subst -nocommands -novariables $v]]}
     proc d v {return [list num $v]}
     proc i v {return [list num $v]}
-    proc l v {return [list s $v]}
-    proc a args {
-        set i -1
-        set r {}
-        foreach v $args {
-            lappend r [incr i] $v
+    proc l v {
+	    switch $v {
+		    null {}
+		    true {set v {b true}}
+		    false {set v {b false}}
+		    default {
+		        error "error: unknown literal '$v'"
+	        }
         }
-        return [list l $r];
+        return $v;
+    }
+    proc a args {
+        return [list L $args];
     }
     proc o args {
         return [list D $args];
     }
 
-    proc huddle ton {
+    proc ton2huddle ton {
         return [list HUDDLE [eval $ton]];
     }
 }
 
-set h [ton::2huddle::huddle $ton];
-puts "$h";
+## set h [ton::2huddle::ton2huddle $ton];
+## puts "$h";
 
